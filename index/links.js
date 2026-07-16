a317dfcca148a0afbd606e22e019638062bbe9be
o-ngr <ashihara.snhr1500@gmail.com>
2026-07-17 02:22:12 +0900

    Add files via upload

---
+++ index/links.js
@@ -0,0 +1,12 @@
+"use strict";
+const LINKS = [
+    { id: 1, title: 'Latest News', desc: '最新情報を確認できますよ', url: '/page/news.html', category: 'extra' },
+    { id: 2, title: 'Game List', desc: '公開中のゲーム一覧', url: '/page/gamelist.html', category: 'tools' },
+    { id: 3, title: 'Tool List', desc: '公開中のツール一覧', url: '/page/toollist.html', category: 'tools' },
+    { id: 4, title: 'Article List', desc: '公開中の記事一覧', url: '/page/articlelist.html', category: 'tools' },
+    { id: 5, title: 'Notepad app', desc: '高機能なメモ帳', url: '/tools/notepad.html', category: 'tools' },
+    { id: 6, title: 'Source code viewer', desc: 'ソースコードを確認', url: '/tools/sauceview.html', category: 'tools' },
+    { id: 7, title: 'Proxy tool', desc: 'プロキシ通信が可能', url: '/tools/proxy.html', category: 'tools' },
+    { id: 8, title: 'Link collection', desc: 'ショートカット一覧 (外部)', url: 'https://a-b-c.3do1.jp/', category: 'tools' },
+    { id: 9, title: 'Link collection 2', desc: 'ショートカット一覧', url: '/page/links.html', category: 'tools' },
+];
